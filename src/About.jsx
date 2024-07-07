76995e1e47f3521aad992af0d37bf422508fe612
shristy gour <sadhnagour170@gmail.com>
2024-07-08 00:32:23 +0530

    add new update

---
+++ src/About.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <>
+      <h1>all about detail</h1>
+    </>
+  );
+}
---
+++ src/css/practise.jsx
@@ -0,0 +1,11 @@
+export default function Fun() {
+  return (
+    <>
+    <div id="main">
+      <div className="f1">EarPhone</div>
+      <div id="div2">Mobile</div>
+      <div id="div3">Laptop</div>
+      </div>
+    </>
+  );
+}
